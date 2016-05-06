db60e1248583034a09393031b93bedceba628a15
Wojtek Tartanus <wtartanus@gmail.com>
2016-05-06 19:30:48 +0100

    Wrote and test room class accessors and readers

--- room.rb
+++ room.rb
@@ -1,35 +1,53 @@
 #Class Room
- #Should have 
-  #type:
-   #normal
-   #king
-  #number of beds:
-   #one single bed
-   #two single
-   #one double
-  #price
-  #view:
-   #sea view
-   #normal view
+class Room
+
+  #Should have 
+   #type: #normal  #king
+   #number of beds: #one single bed  #two single #one double
+   #price
+   #view: #sea view #normal view
   #clean status ( each room is clean from start( u can read and cahnge status), every room status change to dirty when check out)
    #clean
    #dirty
-  #Guest in( u can fully access it but dont set with initialize)
-   #when is no one in just empty variable
-   #when is taken name of guest
-  #Status
-   #hash with keys:
-    #booked
-     #from start set to false
-     #when is booked change value to true
-    #guest name
-     #from begining set to empty string
-     #when is booked change to guest name
 
- #Methods
-  #check type of room
-  #check avability
-  #check type of beds
-  #change clean status
-  #book room for customer
-  #
+   attr_accessor :clean_status, :guest_in, :booking_status
+
+   attr_reader :type, :number_beds, :price, :view
+   
+   def initialize(type,number_beds, price, view)
+    @type = type
+    @number_beds = number_beds
+    @price = price
+    @view = view
+    @clean_status = "clean"
+    @guest_in = ""
+    @booking_status = { status: false, guest_name: ""}
+   end
+   #Guest in( u can fully access it but dont set with initialize)
+    #when is no one in just empty variable
+    #when is taken name of guest 
+   
+   #Status
+    #hash with keys:
+     #booked
+      #from start set to false
+      #when is booked change value to true
+     #guest name
+      #from begining set to empty string
+      #when is booked change to guest name
+   
+    
+    
+   
+   
+   
+
+  #Methods
+   #check type of room
+   #check avability
+   #check type of beds
+   #change clean status
+   #book room for customer
+   #
+
+end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -6,10 +6,51 @@ require_relative( "../room.rb" )
 class TestRomm < MiniTest::Test
 
   def setup
-    room1 = Room.new()
-    room2 = Room.new()
-    room3 = Room.new()
-    room4 = Room.new()
+    @single_room = Room.new("normal", "single", 70, "sea" )
+    @double_single = Room.new("normal", "two single", 60, "normal" )
+    @double = Room.new("king", "double", 200, "sea")
+  end
+
+  def test_create_room_type
+    assert_equal("normal", @single_room.type)
+    assert_equal("normal", @double_single.type)
+    assert_equal("king", @double.type)
+  end
+
+  def test_create_room_number_beds
+    assert_equal("single", @single_room.number_beds)
+    assert_equal("two single", @double_single.number_beds)
+    assert_equal("double", @double.number_beds)
+  end
+
+  def test_create_room_price
+    assert_equal( 70, @single_room.price)
+    assert_equal( 60, @double_single.price)
+    assert_equal( 200, @double.price)
+  end
+
+  def test_create_room_view
+    assert_equal( "sea", @single_room.view)
+    assert_equal( "normal", @double_single.view)
+    assert_equal( "sea", @double.view )
+  end
+
+  def test_room_clean_status
+    assert_equal( "clean", @single_room.clean_status)
+    assert_equal( "clean", @double_single.clean_status)
+    assert_equal( "clean", @double.clean_status )
+  end
+
+  def test_room_guest_in
+    assert_equal( "", @single_room.guest_in)
+    assert_equal( "", @double_single.guest_in)
+    assert_equal( "", @double.guest_in)
+  end
+
+  def test_room_booking_status
+    assert_equal( false, @single_room.booking_status[:status])
+    assert_equal( "", @double_single.booking_status[:guest_name])
+    assert_equal( false, @double.booking_status[:status])
   end
 
 
