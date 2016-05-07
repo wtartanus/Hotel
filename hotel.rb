1fbd7e5784829d73d326a46db27af007da01ef7f
Wojtek Tartanus <wtartanus@gmail.com>
2016-05-07 11:11:34 +0100

    finished booking function

--- hotel.rb
+++ hotel.rb
@@ -1,27 +1,88 @@
+
 #Class Hotel
+class Hotel
+  #Should have:
+  #name #bank #rooms = []  # ????restaurant
+  
+  attr_accessor :name, :bank, :rooms
 
- #Should have:
-  #name
-  #bank
-  #rooms = []
-  # ????restaurant
+  attr_reader :booking_fee
+
+  def initialize(name,bank,rooms)
+    @name = name
+    @bank = bank
+    @rooms = rooms
+    @booking_fee = 50
+  end
+  
 
 
-  #methods
-   #add money
-   
-   #accept booking
-    #charge customer booking fee
-    #add to bank booking fee
+   #methods
+    #add money
+
+    def add_money(payment)
+      @bank += payment
+    end
+
+    #check room avability
+    def is_empty?( room_type )
+      for room in @rooms
+       if room.number_beds == room_type
+        if room.guest_in == "" && room.booking_status[:status] == false
+          return true
+        end
+       end
+      end
+    end
+
+    
     #book choosen room
+    def book_room( guest, room_type)
 
-    #check in 
-     #alocate customer in choosen room
-     #change room avability
+      guest_name = guest.name
 
-    #check out 
-     #change room avability
-     #romove customer from room
-     #charge customer ( customer loose money )
-     #add to bank room price
-     #change room cleaning status
+      for room in @rooms
+        if room.number_beds == room_type
+          room.booking_status[:status] = true
+          room.booking_status[:guest_name] = guest_name
+        end
+      end
+     
+    end
+
+
+
+    
+    #accept booking
+    def booking(guest, room)
+
+      #check room avability
+      room_status = is_empty?( room )
+
+      if room_status == true
+          #charge customer booking fee
+          guest.loose_money( @booking_fee)
+          #add to bank booking fee
+          add_money( @booking_fee )
+          #book choosen room
+          book_room(guest, room)
+
+      else
+        text = "Sorry this room is taken"
+        return text
+      end
+    end
+     
+
+
+     #check in 
+      #alocate customer in choosen room
+      #change room avability
+
+     #check out 
+      #change room avability
+      #romove customer from room
+      #charge customer ( customer loose money )
+      #add to bank room price
+      #change room cleaning status
+end
--- specs/hotel_spec.rb
+++ specs/hotel_spec.rb
@@ -9,6 +9,72 @@ require_relative( "../restaurant.rb" )
 class TestHotel < MiniTest::Test
 
   def setup
+
+    @guest1 = Guest.new("Wojtek", 1000)
+    @guest2 = Guest.new("Basia", 1500)
+
+    single_room = Room.new("normal", "single", 70, "sea" )
+    double_single = Room.new("normal", "two single", 60, "normal" )
+    double = Room.new("king", "double", 200, "sea")
+    
+    rooms = [ single_room, double_single, double ]
+
+    @hotel = Hotel.new("Jurys", 10000, rooms)
+
+  end
+
+  def test_hotel_create_name
+    assert_equal("Jurys", @hotel.name)
+  end
+
+  def test_hotel_create_bank
+    assert_equal(10000, @hotel.bank)
+  end
+
+  # def test_hotel_create_rooms
+  #   binding.pry
+  #   @hotel.rooms
+  #   pretty long array
+  # end
+
+  def test_room_is_empty?
+    test = @hotel.is_empty?( "double" )
+    assert_equal( true, test )
+  end
+
+  def test_add_money
+    @hotel.add_money( @hotel.booking_fee )
+    assert_equal(10050, @hotel.bank )
+  end
+
+  def test_book_room_name
+    @hotel.book_room( @guest1, "single" )
+    assert_equal("Wojtek", @hotel.rooms[0].booking_status[:guest_name])
+  end
+
+  def test_book_room_status
+    @hotel.book_room( @guest1, "single" )
+    assert_equal(true, @hotel.rooms[0].booking_status[:status])
+  end
+
+  def test_booking_guest_loose_money
+    @hotel.booking( @guest1, "double" )
+    assert_equal(950, @guest1.money)
+  end
+
+  def test_booking_hotel_add_money
+    @hotel.booking( @guest1, "single" )
+    assert_equal(10050, @hotel.bank)
+  end
+
+  def test_booking_hotel_room_status
+    @hotel.booking( @guest1, "single" )
+    assert_equal("Wojtek", @hotel.rooms[0].booking_status[:guest_name])
+  end
+
+  def test_booking_hotel_room_status
+    @hotel.booking( @guest1, "single" )
+    assert_equal( true, @hotel.rooms[0].booking_status[:status])
   end
   
 end
